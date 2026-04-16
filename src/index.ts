48b555a021fab63999445028a8f0c3cc06f5d46a
DEPP0804 <deperalta@unicesar.edu.co>
2026-04-15 23:47:48 -0500

    chore: preparando release 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { TaskService } from "./services/taskService";
 
 const service = new TaskService();
 
+console.log("=== [GITFLOW] VERSION: 1.0.0 ===");
+
 service.addTask("Estudiar GitFlow");
 service.addTask("Estudiar Trunk Based Development");
 service.addTask("Crear un proyecto con con Trunk-Based Development");
@@ -9,4 +11,4 @@ service.addTask("Crear un proyecto con con Trunk-Based Development");
 service.completeTask(1);
 service.deleteTask(2);
 
-console.log("[GITFLOW] Tasks:", service.listTasks());
+console.log("[GITFLOW] Lista de Tareas:", service.listTasks());
